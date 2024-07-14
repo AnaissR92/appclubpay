6eae31de086d1aae0f2c2bf66a42ee4dbef4ca84
Javier Bianchet <javierbianchet@gmail.com>
2024-07-14 08:21:14 -0500

    Se agrego Pos y Delivery Views/controller

--- app/Http/Controllers/DeliveryController.php
+++ app/Http/Controllers/DeliveryController.php
@@ -6,6 +6,12 @@ use Illuminate\Http\Request;
 
 class DeliveryController extends Controller
 {
+    public function __construct()
+    {
+
+        $this->middleware('auth');
+    }
+
     public function index()
     {
         // Lógica para la página de Delivery
