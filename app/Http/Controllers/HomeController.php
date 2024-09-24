bc0da53516a31aae2e501297f10624e0e6e97470
148961 <u116825838@infong-eu-cl0167.clienthosting.eu>
2024-09-24 15:54:40 +0200

    correcion Error alcargar el Home(Undefined variable )

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -47,6 +47,7 @@ class HomeController extends Controller
 
 
         if (!$user->hasRole('Super-Admin')) {
+            $count['subscriptions']=array(); //array
 
             $restaurant_id = isset($restaurant->id) ? $restaurant->id : 0;
 
